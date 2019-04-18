1d23fa8d6861b4a120f033af8c3d358e49950237
Jason Viger <jason.viger@gmail.com>
2019-04-18 08:42:50 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'products/index'
+  root 'products#index'
   post '/' => 'products#add'
 
   # The priority is based upon order of creation: first created -> highest priority.
